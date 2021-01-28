c43e66f141271f669b12ebc51f212f4306f55ec7
Ankit-gitBook <77018870+Ankit-gitBook@users.noreply.github.com>
2021-01-28 01:26:11 -0800

    Add files via upload

---
+++ src/components/UserProfile.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUserProfile } from '../actions/profile';
+
+class UserProfile extends Component {
+  componentDidMount() {
+    const { match } = this.props;
+
+    if (match.params.userId) {
+      // dispatch an action
+      this.props.dispatch(fetchUserProfile(match.params.userId));
+    }
+  }
+
+  checkIfUserIsAFriend = () => {
+    console.log('this.props', this.props);
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.to_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
+
+  render() {
+    const {
+      match: { params },
+      profile,
+    } = this.props;
+    console.log('this.props', params);
+    const user = profile.user;
+
+    if (profile.inProgress) {
+      return <h1>Loading!</h1>;
+    }
+
+    const isUserAFriend = this.checkIfUserIsAFriend();
+
+    return (
+      <div className="settings">
+        <div className="img-container">
+          <img
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-dp"
+          />
+        </div>
+
+        <div className="field">
+          <div className="field-label">Name</div>
+          <div className="field-value">{user.name}</div>
+        </div>
+
+        <div className="field">
+          <div className="field-label">Email</div>
+          <div className="field-value">{user.email}</div>
+        </div>
+
+        <div className="btn-grp">
+          {!isUserAFriend ? (
+            <button className="button save-btn">Add Friend</button>
+          ) : (
+            <button className="button save-btn">Remove Friend</button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ profile, friends }) {
+  return {
+    profile,
+    friends,
+  };
+}
+export default connect(mapStateToProps)(UserProfile);
---
+++ src/helpers/urls.js
@@ -0,0 +1,11 @@
+const API_ROOT = 'http://codeial.com:8000/api/v2';
+
+export const APIUrls = {
+  login: () => `${API_ROOT}/users/login`,
+  signup: () => `${API_ROOT}/users/signup`,
+  editProfile: () => `${API_ROOT}/users/edit`,
+  fetchPosts: (page = 1, limit = 25) =>
+    `${API_ROOT}/posts?page=${page}&limit=${limit}`,
+  userProfile: (userId) => `${API_ROOT}/users/${userId}`,
+  userFriends: () => `${API_ROOT}/friendship/fetch_user_friends`,
+};
